67836293b9b820a3a7d10c69ea213fb023a11796
Thobiy <tobiemmanuel19@gmail.com>
2022-06-23 23:57:03 +0100

    first commit

---
+++ Tobi.js
@@ -0,0 +1,29 @@
+
+
+/*let Name = "Abolarin Emmanuel Tobi"
+console.log(Name);
+document.write("Tobi", "\n");
+document.writeln("25");
+document.write("5'7")  ; */
+
+
+
+
+let Yname= "Abolarin Tobi";
+let Height = "5'7";
+let Country = "Nigeria";
+alert("Name is " + Yname  + "\n" + "Height: " + Height + "\n" + "Country: " + Country  );
+
+
+
+function MyDetail(
+    fullName ="Abolarin Tobi",
+   Age = "25",
+   Height = "5'7"){
+   // let result;
+   result = fullName + "\n" +Age + "\n" + Height ;
+//return detail
+
+}
+//let result = MyDetail();
+console.log(result)
